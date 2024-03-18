f16e8609beb2b0cb4dca9e98a0809a1052eb2646
Hamaddddd <107883286+Hamaddddd@users.noreply.github.com>
2024-03-18 11:02:45 +0500

    login app  lab 05 task

--- Lab Tasks/LoginApp/App.js
+++ Lab Tasks/LoginApp/App.js
@@ -1,11 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+
+
 
 export default function App() {
+
+  let [username,setUsername] = useState("");
+  let [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <Text style = {{fontSize: 30,color:"blue",fontWeight: "bold"}}
       >Login App</Text>
+
+      <TextInput
+        style={styles.input}
+        keyboardType= "default"
+        placeholder='Enter Username Here'
+        placeholderTextColor="black"
+        onChangeText={(text) => {
+          setUsername(text)
+        }}
+        value={username}
+      />
+      
+      <TextInput
+        style={styles.input}
+        keyboardType= "visible-password"
+        placeholder='Enter Password Here'
+        placeholderTextColor="black"
+        onChangeText={(text) => {
+          setPassword(text)
+        }}
+        value={password}
+      />
+
+      <Button
+        color="green"
+        title='Submit Now'
+        onPress={() => {
+          if(username == "cspeople" & password == "computerscience"){
+            alert("Login Successful !!! Great");
+          }
+          else{
+            alert("WARNING : Username Or Password Incorrect !!")
+          }
+
+        }}
+      />
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +62,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    width: '80%',
   },
 });
